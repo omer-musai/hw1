409db9f7a3764031be2002e9abc1a3a1319dba2b
omer-musai <68822871+omer-musai@users.noreply.github.com>
2021-05-02 22:13:13 +0300

    adding map.c

---
+++ mtm_map/map.c
@@ -0,0 +1,59 @@
+
+#include "map.h"
+
+
+
+
+Map mapCreate(copyMapDataElements copyDataElement,
+              copyMapKeyElements copyKeyElement,
+              freeMapDataElements freeDataElement,
+              freeMapKeyElements freeKeyElement,
+              compareMapKeyElements compareKeyElements)
+              {
+                  
+
+
+
+              }
+
+
+void mapDestroy(Map map)
+{
+
+}
+Map mapCopy(Map map)
+{
+
+}
+int mapGetSize(Map map)
+{
+
+}
+bool mapContains(Map map, MapKeyElement element)
+{
+
+}
+MapResult mapPut(Map map, MapKeyElement keyElement, MapDataElement dataElement)
+{
+
+}
+MapDataElement mapGet(Map map, MapKeyElement keyElement)
+{
+
+}
+MapResult mapRemove(Map map, MapKeyElement keyElement)
+{
+
+}
+MapKeyElement mapGetFirst(Map map)
+{
+
+}
+MapKeyElement mapGetNext(Map map)
+{
+
+}
+MapResult mapClear(Map map)
+{
+
+}
