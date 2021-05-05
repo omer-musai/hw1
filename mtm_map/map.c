7aee3e58700cf49fb9162a1aed690f02dc8f0245
Shay <ShayHacohen14@gmail.com>
2021-05-05 22:16:42 +0300

    Adding some initial on the implementations of a couple of the map ADT's basic associated methods.

--- mtm_map/map.c
+++ mtm_map/map.c
@@ -1,25 +1,70 @@
 
 #include "map.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 
+//TODO: Implement linked list.
 
+typedef struct LinkedListNode
+{
+    MapDataElement value;
+    MapKeyElement key;
+    struct LinkedListNode *next;
+} MapNode;
+
+struct Map_t
+{
+    MapDataElement iterator;
+    MapDataElement length; //Current length.
+    MapNode *elements;
+    
+    copyMapDataElements copyDataElement;
+    copyMapKeyElements copyKeyElement;
+    compareMapKeyElements compareElements;
+    freeMapDataElements freeDataElement;
+    freeMapKeyElements freeKeyElement;
+};
 
 Map mapCreate(copyMapDataElements copyDataElement,
               copyMapKeyElements copyKeyElement,
               freeMapDataElements freeDataElement,
               freeMapKeyElements freeKeyElement,
               compareMapKeyElements compareKeyElements)
-              {
-                  
+{
+                Map map = malloc(sizeof(*map));
+                if(map == NULL)
+                {
+                    return NULL;
+                }
+                map->elements = NULL;
 
-
-
-              }
+                map->copyDataElement = copyDataElement;
+                map->copyKeyElement = copyKeyElement;
+                map->compareElements = compareKeyElements;
+                map->freeDataElement = freeDataElement;
+                map->freeKeyElement = freeKeyElement;
+                
+                return map;
+}
 
 
 void mapDestroy(Map map)
 {
 
+    while (map->elements != NULL)
+    {
+        map->freeKeyElement(map->elements->key);
+        map->freeDataElement(map->elements->value);
+
+        MapNode *tmp = map->elements->next;
+        
+        free(map->elements);
+        
+        map->elements = tmp;
+    }
+    
+    free(map);
 }
 Map mapCopy(Map map)
 {
@@ -35,7 +80,7 @@ bool mapContains(Map map, MapKeyElement element)
 }
 MapResult mapPut(Map map, MapKeyElement keyElement, MapDataElement dataElement)
 {
-
+    
 }
 MapDataElement mapGet(Map map, MapKeyElement keyElement)
 {
