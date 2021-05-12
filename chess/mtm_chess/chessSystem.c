9651ed991ab502eaf19c4540e16aac4132530d13
omer-musai <68822871+omer-musai@users.noreply.github.com>
2021-05-12 17:56:29 +0300

    implementation of tournament and map methods and few more functions

--- chess/mtm_chess/chessSystem.c
+++ chess/mtm_chess/chessSystem.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 #include "chessSystem.h"
 #include "map.h"
 
+//TODO: Check if we're supposed to specify a constant length or not.
+#define LOCATION_MAX_LENGTH 1000
+
 struct chess_system_t
 {
-    Map tournaments;
-              
+    Map tournaments;      
+    Map games;      
 };
 
 typedef struct game_t
 {
     unsigned int game_id;
+    unsigned int tournament_id;
     int id_player1;
     int id_player2;
     Winner score;
@@ -20,26 +25,145 @@ typedef struct game_t
 
 } Game;
 
+
 typedef struct tournament_t
 {
     unsigned int tournament_id;
     char* location;
-    Map games;
+    Map game_ptrs;
     int winner;
 
 } Tournament;
 
-//function declaration
+//functions declaration
 bool invalidLocation(char* tournament_location);
+int gameId(Map games);
 
-ChessSystem chessCreate()
-{    
+
+//methods declaration
+static MapDataElement copyTournamentData(MapDataElement tournament);
+static MapKeyElement copyIntegerId(MapKeyElement key);
+static void freeIntegerKey(MapKeyElement key);
+static void freeTournamentData(MapDataElement tournament);
+static int compareIntegerKeys(MapKeyElement key1, MapKeyElement key2);
+static void freeGameData(MapDataElement game);
+static MapDataElement copyGameData(MapDataElement game);
+
+
+//Tournament and Game map methods:
+static MapDataElement copyTournamentData(MapDataElement tournament)
+{
+    if (tournament == NULL)
+    {
+        return NULL;
+    }
+    Tournament *copy = malloc(sizeof(*copy));
+    if (copy == NULL)
+    {
+        return NULL;
+    }
+
+    Tournament src = *((Tournament*)tournament);
+    copy->tournament_id = src.tournament_id;
+    copy->location = malloc(sizeof(*(copy->location)) * LOCATION_MAX_LENGTH);
+    strcpy(copy->location, src.location);
+    copy->game_ptrs = mapCopy(src.game_ptrs);
+    copy->winner = src.winner;
+
+    return copy;
+}
+
+static MapKeyElement copyIntegerId(MapKeyElement key)
+{
+    if (!key) 
+    {
+        return NULL;
+    }
+    int *copy = malloc(sizeof(*copy));
+    
+    if (!copy)
+    {
+        return NULL;
+    }
+    
+    *copy = *(int *) key;
+    return copy;
+}
+
+static void freeIntegerKey(MapKeyElement key)
+{
+    //Our key is an integer so nothing needs to be done.
+}
+
+static void freeTournamentData(MapDataElement tournament)
+{
+    if(!tournament)
+    {
+        return NULL;
+    }
+    Tournament tournamentToFree = *(Tournament *) tournament;
+    
+    free(tournamentToFree.location);
+    mapDestroy(tournamentToFree.game_ptrs);
+    free(tournament);
+    
+    return;
+}
+
+static int compareIntegerKeys(MapKeyElement key1, MapKeyElement key2) 
+{
+    return (*(int *) key1 - *(int *) key2);
+}
+
+static void freeGameData(MapDataElement game)
+{
+    free(game);
+    return;
+}
+
+static MapDataElement copyGameData(MapDataElement game)
+{
+    if(!game)
+    {
+        return NULL;
+    }
+    Game *copy = malloc(sizeof(*copy));
+    if(copy==NULL)
+    {
+        return NULL;
+    }
+
+    Game src = *((Game *) game);
+
+    copy->game_id = src.game_id;
+    copy->game_time = src.game_time;
+    copy->id_player1 = src.id_player1;
+    copy->id_player2 = src.id_player2;
+    copy->score = src.score;
+    copy->tournament_id = src.tournament_id;
+
+    return copy;
+}
+
+ChessSystem chessCreate()                 
+{   
+    Map tournaments = mapCreate(&copyTournamentData,
+                                &copyIntegerId,
+                                &freeTournamentData,
+                                &freeIntegerKey,
+                                &compareIntegerKeys);
+    Map games = mapCreate(&copyGameData,
+                        &copyIntegerId,
+                        &freeGameData,
+                        &freeIntegerKey,
+                        &compareIntegerKeys);
+
     ChessSystem chess_system = malloc(sizeof(*chess_system));
     if(chess_system == NULL)
     {
         return NULL;
     }
-
+    
     return chess_system;        
 }
 
@@ -51,31 +175,39 @@ void chessDestroy(ChessSystem chess)
     }
 
     mapDestroy(chess->tournaments);
+    mapDestroy(chess->games);
     free(chess);
     return;
-	
 }
 
 ChessResult chessAddTournament (ChessSystem chess, int tournament_id,
                                 int max_games_per_player, const char* tournament_location)
 {
-    MapResult result = mapPut(chess, tournament_id, tournament_location);
-    
-    if(result == MAP_ITEM_DOES_NOT_EXIST)
-    {
-        return CHESS_INVALID_ID; 
-    }
-    else if(result == MAP_ITEM_ALREADY_EXISTS)
-    {
-        return CHESS_TOURNAMENT_ALREADY_EXISTS;
-    }
-    else if(invalidLocation(tournament_location))
+    if(invalidLocation(tournament_location))
     {
         return CHESS_INVALID_LOCATION;
     }
-    else if(max_games_per_player <= 0)
+    if(max_games_per_player <= 0)
     {
         return CHESS_INVALID_MAX_GAMES;
+    }
+    if(tournament_id <= 0)
+    {
+        return CHESS_INVALID_ID; 
+    }
+
+    Tournament *tournament = malloc(sizeof(*tournament));
+    if(tournament == NULL)
+    {
+        return CHESS_NULL_ARGUMENT;
+    }
+
+    MapResult result = mapPut(chess->tournaments, tournament_id, tournament);
+    
+    if(result == MAP_ITEM_ALREADY_EXISTS)
+    {   
+        free(tournament);
+        return CHESS_TOURNAMENT_ALREADY_EXISTS;
     }
     else
     {
@@ -86,7 +218,47 @@ ChessResult chessAddTournament (ChessSystem chess, int tournament_id,
 ChessResult chessAddGame(ChessSystem chess, int tournament_id, int first_player,
                          int second_player, Winner winner, int play_time)
 {
+    if(play_time < 0)
+    {
+        return CHESS_INVALID_PLAY_TIME;
+    }
+    else if(tournament_id <= 0 || first_player <= 0 || second_player <= 0)
+    {
+        return CHESS_INVALID_ID;
+    }
+    else if(!mapContains(chess->tournaments, tournament_id))
+    {
+        return CHESS_TOURNAMENT_NOT_EXIST;
+    }
+    else if(chessEndTournament(chess, tournament_id) == CHESS_TOURNAMENT_ENDED)
+    {
+        return CHESS_TOURNAMENT_ENDED;
+    }
+
+    Game* gameToAdd = malloc(sizeof(*gameToAdd));
     
+    gameToAdd->tournament_id = tournament_id;
+    gameToAdd->id_player1 = first_player;
+    gameToAdd->id_player2 = second_player;
+    gameToAdd->score = winner;
+    gameToAdd->game_time = play_time;
+
+    
+
+    MapResult result = mapPut(chess->games, gameToAdd->game_id, gameToAdd);
+
+    //set game id to be the size of the games map (there is no removals of games from the map so it's works)
+    gameToAdd->game_id = gameId(chess->games);
+
+    if(result == MAP_ITEM_ALREADY_EXISTS)
+    {   
+        free(gameToAdd);
+        return CHESS_TOURNAMENT_ALREADY_EXISTS;
+    }
+    else
+    {
+        return CHESS_SUCCESS;
+    }
 }
 
 ChessResult chessRemoveTournament (ChessSystem chess, int tournament_id)
@@ -142,3 +314,10 @@ bool invalidLocation(char* tournament_location)
         return true;
     }    
 }
+
+int gameId(Map games)
+{
+    return mapGetSize(games);
+}
+
+ 
