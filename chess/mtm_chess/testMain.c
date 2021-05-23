bbe4113ebf128b3397bd703a1be1c170537634ee
Shay <ShayHacohen14@gmail.com>
2021-05-24 02:11:22 +0300

    Adding a test main & modified version of the CMakeLists to being our bug crusade.

---
+++ chess/mtm_chess/testMain.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "chessSystem.h"
+
+int main() {
+    ChessSystem chess = chessCreate();
+    if (chess == NULL)
+    {
+        printf("Null :(\n");
+    }
+    else
+    {
+        printf("Hot damn!\n");
+        chessAddTournament(chess, 1, 3, "Tokyo");
+        chessAddGame(chess, 1, 13, 31, SECOND_PLAYER, 15);
+        chessEndTournament(chess, 1);
+        chessSaveTournamentStatistics(chess, "C:\\Users\\ShayH\\OneDrive\\Desktop\\hw1");
+        chessDestroy(chess);
+    }
+    return 0;
+}
